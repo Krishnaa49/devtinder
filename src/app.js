6301248bc8afddc07f9eede4cf2480f8361fdb80
krishnaa49 <mk5612330@gmail.com>
2025-07-04 00:23:17 +0530

    New Test Get route added

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 
 
-app.use('/',(req,res)=>{
+app.use('/users',(req,res)=>{
     res.send("dfghjk");
 })
 
+app.get('/test' , (req,res,next)=>{
+    // res.send();
+    next();
+},
+(req,res)=>{
+    res.send("Hellow There !")
+}
+)
 
 app.listen(3000);
